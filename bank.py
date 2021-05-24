ec191bd40f83e469fb8e53a4f66009d5cb6f63c3
student <ednnamukami@gmail.com>
2021-05-24 20:36:48 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,15 +1,34 @@
 class BankAccount:
 
-    def __init__(self,accountNumber,amount):
-        self.acccountNumber=accountNumber
-        self.amount=amount
+    def __init__(self,name,phonenumber):
+        self.name=name
+        self.balance=0
+        self.phonenumber=phonenumber
+
+
+    def  show_balance(self):
+        return f"Your balance is {self.balance}"
 
    
-    def accountNumber(self):
+    def name(self):
         return f"The amount has been sent to {self.acccountNumber}" 
 
-    def money(self):
-        return f"Your balance is {self.amount}"
+    def deposit(self):
+        return f"Your balance is {self.balance}"
+    def deposit(self,amount):
+        self.balance+=amount
+        return self.show_balance()
+    def withdraw(self,amount):
+        if amount>self.balance :
+            return f"your balance is {self.balance} you cannot withdraw {amount}"
+        else:
+            self.balance-=amount
+            return self.show_balance()
+    def borrow(self,amount):
+            return f"your request has been confirmed , you have recieved {self.amount}"
+    def pay_back(self,amount):
+            return f"you are expected to pay back this amount {amount} by 22nd july 2024"
+            
 
 
 
--- student.py
+++ student.py
@@ -1,12 +1,20 @@
 class Student:
     school="Akiachix"
-
-    def __init__(self,name,age):
-        self.name=name
+    def __init__(self,first_name,last_name,age,initials,year_of_birth):
+        
         self.age=age
-    
-    def speak(self):
-        return f"hello class, my name is {self.name}"
-    
+        self.first_name=first_name
+        self.last_name=last_name
+        self.age=age
+        self.year_of_birth=year_of_birth
+        self.intials=initials
+
+    def greet (self):
+        return  f"hello my name is {self.first_name} {self.last_name}"  
     def year_of_birth(self):
-        return f"She was born on {2021-self.age}"
+        return
+
+    def initials (self):
+        return f"{self.first_name[0]} {self.last_name[0]}"
+
+    
